62ca96bdf37e239e73335b6376c7f08b1d0badac
RetroDriven <44145655+RetroDriven@users.noreply.github.com>
2019-11-04 19:24:26 -0500

    Verbage Updates

--- Update_RetroDriven.sh
+++ Update_RetroDriven.sh
@@ -22,10 +22,10 @@
 ###### Disclaimer / Legal Information ######
 By downloading ROM injected Core/RBF files you are agreeing to the following:
 
-* You are responsible for checking your local laws regarding the use of the ROM injected Core/RBF files.
-* You are authorized to own a license to use any the ROM injected Core/RBF file that you download.
+* You are responsible for checking your local laws regarding the use of the ROMs associated with the injected Core/RBF files.
+* You are authorized/licensed to own/use the ROMs associated with the injected Core/RBF files that you download.
 * You will not distribute any of these files without the appropriate permissions.
-* You own the original Arcade PCB for each ROM injected Core/RBF files that you download.
+* You own the original Arcade PCB for each ROM injected Core/RBF file that you download.
 * I take no responsibility for any data loss or anything, use the script at your own risk.
 '
 
